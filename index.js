e28e7255dceb4fd223e01ff8085ab32ec1da3e63
rifataktchy <rifataktchy@gmail.com>
2024-12-24 23:33:10 +0100

    six commit

--- index.js
+++ index.js
@@ -59,7 +59,19 @@ async function run() {
     const eventCollection = client.db('merathonDB').collection('event');
     const registerCollection = client.db('merathonDB').collection('register');
 
+//auth related apis
+app.post('/jwt', async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' });
 
+  res
+      .cookie('token', token, {
+          httpOnly: true,
+          secure: false, //for localhost
+      })
+      .send({ success: true })
+
+});
 
 //event related apis
 app.post("/events", async (req, res) => {
